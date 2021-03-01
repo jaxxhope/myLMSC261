188e9d3d6f3f9ee819caea1d9a94aef21c1fc692
JacquelynHope <78001019+JacquelynHope@users.noreply.github.com>
2021-03-01 04:28:51 -0500

    mice update

---
+++ mice/README copy.md
@@ -0,0 +1,5 @@
+# Assignment-mice
+- This assignment did not go very well for me, I thought I was off to a great start although it ended up really being a challenge and I didn't get it this time around
+- I thought I understood based on the new information I learned during the most recent class session but maybe I misunderstood
+- Was unsure how to get that exact spacing and three separate lines with one single print function
+- Hoping I will get clarity on this with today's upcoming class session
---
+++ mice/{Hope}mice.py
@@ -0,0 +1,24 @@
+Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+WARNING: The system preference "Prefer tabs when opening documents" is set to
+"Always". This will cause various problems with IDLE. For the best experience,
+change this setting when running IDLE (via System Preferences -> Dock).
+>>> count = 0
+>>> while (count < 3): 	
+  count = count + 1
+  print("  3")
+  print("  	Blind")
+  print("   		Mice")
+
+  
+  3
+  	Blind
+   		Mice
+  3
+  	Blind
+   		Mice
+  3
+  	Blind
+   		Mice
+>>> 
